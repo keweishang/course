8ea3da37d9805d0e767b8fb62ff3c299bbd91dbb
Kewei Shang <kewei.shang@gmail.com>
2016-06-04 13:06:25 +0200

    Finished the special copy exercise

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -15,41 +15,82 @@ import commands
 """Copy Special exercise
 """
 
+
 # +++your code here+++
 # Write functions and modify main() to call them
+def get_special_filepaths(dirname):
+    result = []
 
+    filenames = os.listdir(dirname);
+    for filename in filenames:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            result.append(os.path.abspath(os.path.join(dirname, filename)))
+
+    return result
+
+
+def copy_to(special_filepaths, todir):
+    """Copy multiple files to a directory"""
+    if not os.path.exists(todir):
+        os.mkdir(todir)
+    for filepath in special_filepaths:
+        shutil.copy(filepath, todir)
+
+
+def zip_to(paths, zipfile):
+    """Zip multiple files with the given name"""
+    cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
+    print 'Execute command', cmd
+
+    # If the command has a problem, the status is not 0, print the error message to stderr
+    (status, output) = commands.getstatusoutput(cmd)
+    if status:
+        sys.stderr.write(output)
+        sys.exit(1)
 
 
 def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
+    # This basic command line argument parsing code is provided.
+    # Add code to call your functions below.
 
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
-    sys.exit(1)
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
+    if not args:
+        print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+        sys.exit(1)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    # todir and tozip are either set from command line
+    # or left as the empty string.
+    # The args array is left just containing the dirs.
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+    tozip = ''
+    if args[0] == '--tozip':
+        tozip = args[1]
+        del args[0:2]
 
-  if len(args) == 0:
-    print "error: must specify one or more dirs"
-    sys.exit(1)
+    if len(args) == 0:
+        print "error: must specify one or more dirs"
+        sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
-  
+    # +++your code here+++
+    # Call your functions
+    special_filepaths = []
+    for dir in args:
+        special_filepaths.extend(get_special_filepaths(dir))
+
+    if todir:
+        copy_to(special_filepaths, todir)
+    elif tozip:
+        zip_to(special_filepaths, tozip)
+    else:
+        print '\n'.join(special_filepaths)
+
+
 if __name__ == "__main__":
-  main()
+    main()

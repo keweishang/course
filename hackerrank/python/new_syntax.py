2ad6baf37cc4886387a90cd3023e23a44ff033f9
Kewei Shang <kewei.shang@gmail.com>
2016-06-14 00:31:48 +0200

    delete hackerrank from course because it is now in hackerrank repo

--- hackerrank/python/new_syntax.py
+++
@@ -1,5 +0,0 @@
-# Separate a string by sep from the rear 
-name,space,price = raw_input().rpartition(' ')
-
-# Iterate through dict items, each item is a tuple
-for item_name, net_price in my_order.items():
